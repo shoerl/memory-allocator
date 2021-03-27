0fbe0b5d62c55b402b8531bccbfc06552f71d68a
shoerl <hoerl.s@northeastern.edu>
2021-03-27 13:35:31 -0400

    tears

--- hwx_malloc.c
+++ hwx_malloc.c
@@ -21,246 +21,111 @@
 //    provided xv6 allocator.
 //  - Implement the "realloc" function for this allocator.
 
-const size_t PAGE_SIZE = 4096;
-static hm_stats stats; // This initializes the stats to 0.
-free_block* free_list = NULL;
+typedef long Align;
+
+union header {
+  struct {
+    union header *ptr;
+    unsigned int size;
+  } s;
+  Align x;
+};
+
+typedef union header Header;
+
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-
-long
-free_list_length()
-{
-	long ii = 0;
-	free_block* head;
-	for (head=free_list; head != NULL; head=head->next) {
-		ii += 1;
-	}
-    return ii;
-}
-
-hm_stats*
-hgetstats()
-{
-    stats.free_length = free_list_length();
-    return &stats;
-}
-
-void
-hprintstats()
-{
-    stats.free_length = free_list_length();
-    fprintf(stderr, "\n== husky malloc stats ==\n");
-    fprintf(stderr, "Mapped:   %ld\n", stats.pages_mapped);
-    fprintf(stderr, "Unmapped: %ld\n", stats.pages_unmapped);
-    fprintf(stderr, "Allocs:   %ld\n", stats.chunks_allocated);
-    fprintf(stderr, "Frees:    %ld\n", stats.chunks_freed);
-    fprintf(stderr, "Freelen:  %ld\n", stats.free_length);
-}
+static Header base;
+static Header *freep;
 
 static
-size_t
-div_up(size_t xx, size_t yy)
+void
+xfree_helper(void *ap)
 {
-    // This is useful to calculate # of pages
-    // for large allocations.
-    size_t zz = xx / yy;
+  Header *bp, *p;
 
-    if (zz * yy == xx) {
-        return zz;
-    }
-    else {
-        return zz + 1;
-    }
+  bp = (Header*)ap - 1;
+  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+    if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+      break;
+  if(bp + bp->s.size == p->s.ptr){
+    bp->s.size += p->s.ptr->s.size;
+    bp->s.ptr = p->s.ptr->s.ptr;
+  } else
+    bp->s.ptr = p->s.ptr;
+  if(p + p->s.size == bp){
+    p->s.size += bp->s.size;
+    p->s.ptr = bp->s.ptr;
+  } else
+    p->s.ptr = bp;
+  freep = p;
 }
 
 void
-coalesce()
+xfree(void* ap)
 {
-	free_block* prev = free_list;
-	if (prev == NULL) {
-		return;
-	}
-	free_block* head = free_list->next;
-	int count = 0;
-	for (; head != NULL; head=head->next) {
-		void* tmp = ((void*) prev) + prev->size;
-		// if these are adjacent
-		if ((uintptr_t) tmp == (uintptr_t) head) {
-			count++;
-			// add size to prev
-			prev->size += head->size;
-			// set next of prev to next of current (aka remove current from list)
-			prev->next = head->next;
-		}
-		// set prev for next iteration
-		prev = head;
-	}
-	if (count > 0) {
-		coalesce();
-	}
+  pthread_mutex_lock(&lock);
+  xfree_helper(ap);
+  pthread_mutex_unlock(&lock);
 }
 
-
-void
-insert(free_block* to_insert)
+static Header*
+morecore(size_t nu)
 {
-	free_block* head;
-	free_block* prev = NULL;
-	for (head = free_list; head != NULL; head=head->next) {
-		// if these are adjacent
-		if ((uintptr_t) to_insert < (uintptr_t) head) {
-			if (prev != NULL) {
-				prev->next = to_insert;
-			}
-			to_insert->next = head;
-			break;
-		}
-		// set prev for next iteration
-		prev = head;
-	}
-	// if we are inserting it to first slot fix free list pointer
-	if (prev == NULL) {
-		free_list = to_insert;
-	}
-}
+  char *p;
+  Header *hp;
 
-void
-add_or_discard(free_block* curr, free_block* prev)
-{
-	if (curr->size > sizeof(free_block)) {
-		if (prev == NULL) {
-			// if we are messing with head of list
-			free_list = curr;
-		} else {
-			prev->next = curr;
-		}
-	} else {
-		if (prev == NULL) {
-			free_list = NULL;
-		} else {
-			prev->next = curr->next;
-		}
-	}
+  if(nu < 4096)
+    nu = 4096;
+  // TODO: Replace sbrk use with mmap
+  p = mmap(0, nu * sizeof(Header), PROT_READ|PROT_WRITE,
+           MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+  if(p == (char*)-1)
+    return 0;
+  hp = (Header*)p;
+  hp->s.size = nu;
+  xfree_helper((void*)(hp + 1));
+  return freep;
 }
 
 void*
-xmalloc(size_t size)
+xmalloc(size_t nbytes)
 {
-	int ret = pthread_mutex_lock(&lock);
-	assert(ret != -1);
- 	size += sizeof(size_t);
-	stats.chunks_allocated += 1;
-	if (size < PAGE_SIZE) {
-		free_block* prev = NULL;
-		free_block* curr = free_list;
-		for (; curr != NULL; curr=curr->next) {
-			if (curr->size >= size) {
-				size_t oldSize = curr->size;
-				block_header* block = (void*) curr;
-				block->size = size;
-				curr = ((void*) curr) + size;
-				curr->size = oldSize - size;
-				add_or_discard(curr, prev);
-				ret = pthread_mutex_unlock(&lock);
-				assert(ret != -1);
-				return ((void*) block) + sizeof(size_t); 
+  Header *p, *prevp;
+  unsigned int nunits;
 
-			}
-			prev = curr;
-		}
-		// no more space, map extra page 
-		stats.pages_mapped += 1;
-		block_header* new_header = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-		new_header->size = size;
-		free_block* new_block = ((void*) new_header) + size;
-		new_block->size = PAGE_SIZE - size;
-		if (new_block->size > sizeof(free_block)) {
-			insert(new_block);
-		}
-		ret = pthread_mutex_unlock(&lock);
-		assert(ret != -1);
-		return ((void*) new_header) + sizeof(size_t);
-	} else {
-		void* block = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-		((block_header*) block)->size = size - sizeof(size_t);
-		stats.pages_mapped += div_up(size, PAGE_SIZE);
-		ret = pthread_mutex_unlock(&lock);
-		assert(ret != -1);
-		return block + sizeof(size_t);
-	}
-}
-
-void
-xfree(void* item)
-{
-	int ret = pthread_mutex_lock(&lock);
-	assert(ret != -1);
-    // TODO: replace this with free
-    stats.chunks_freed += 1;
-    void* item_f = item - sizeof(size_t);
-    size_t size = *((size_t*) item_f);
-    // to account for size we keep track of
-    if (size < PAGE_SIZE) {
-    	free_block* new = item_f;
-    	new->size = size;
-    	insert(new);
-    	coalesce();
-    } else {
-    	size += sizeof(size_t);
-    	stats.pages_unmapped += div_up(size, PAGE_SIZE);
-    	munmap(item_f, size);
+  pthread_mutex_lock(&lock);
+  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
+  if((prevp = freep) == 0){
+    base.s.ptr = freep = prevp = &base;
+    base.s.size = 0;
+  }
+  for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
+    if(p->s.size >= nunits){
+      if(p->s.size == nunits)
+        prevp->s.ptr = p->s.ptr;
+      else {
+        p->s.size -= nunits;
+        p += p->s.size;
+        p->s.size = nunits;
+      }
+      freep = prevp;
+      pthread_mutex_unlock(&lock);
+      return (void*)(p + 1);
     }
-    ret = pthread_mutex_unlock(&lock);
-    assert(ret != -1);
+    if(p == freep) {
+      if((p = morecore(nunits)) == 0) {
+        pthread_mutex_unlock(&lock);
+        return 0;
+      }
+    }
+  }
 }
 
 void*
-xrealloc(void* prev, size_t bytes)
+xrealloc(void* prev, size_t nn)
 {
-	prev = prev - sizeof(size_t);
-	block_header* prev_block = prev;
-	size_t prev_size = prev_block->size;
-	if (bytes == prev_size) {
-		return ((void*) prev_block);
-	}
-	void* tmp = ((void*) prev) + prev_block->size;
-	free_block* other_prev = NULL;
-	for (free_block* head = free_list; head != NULL; head=head->next) {
-		// if these are adjacent
-		// TODO: if extra space in free block head, break it up
-		if ((uintptr_t) tmp == (uintptr_t) head && bytes <= (head->size + prev_size)) {
-			size_t space_needed = bytes - prev_size;
-			size_t leftover_space = head->size - space_needed;
-			prev_block->size += space_needed;
-			head = ((void*) head) + space_needed;
-			head->size = leftover_space;
-			add_or_discard(head, other_prev);
-			return prev + sizeof(size_t);
-		}
-		// set prev for next iteration
-		other_prev = head;
-	}
-    void* new_space = xmalloc(bytes);
-    memcpy(new_space, prev, prev_size);
-    ((block_header*) new_space)->size = bytes;
-    xfree(prev + sizeof(size_t));
-    return new_space + sizeof(size_t);
+	void* new_space = xmalloc(nn);
+	memcpy(new_space, prev, nn);
+	xfree(prev);
+	return new_space;	
 }
-
-/*
-int
-main(int argc, char* argv[])
-{
-    long* xs = xmalloc(30 * sizeof(long));
-    for (int i = 0; i < 30; i++) {
-    	xs[i] = i + 1;
-    }
-    xs = xrealloc(xs, 50 * sizeof(long));
-    for (int i = 0; i < 30; i++) {
-    	printf("at index %i : %li\n", i, xs[i]);
-    }
-	xs[32] = 500;
-	printf("%li\n", xs[32]);
-	hprintstats();
-	xfree(xs);
-}
-*/
