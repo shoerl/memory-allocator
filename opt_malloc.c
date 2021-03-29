ca46692f5175c29bf68eb7e9f4eeee0815c59587
shoerl <hoerl.s@northeastern.edu>
2021-03-29 19:50:47 -0400

    lets goooooo

--- opt_malloc.c
+++ opt_malloc.c
@@ -11,6 +11,7 @@ typedef struct page_header page_header;
 struct page_header {
 	size_t size; // 8 bytes
 	page_header* next; // 8 bytes
+	page_header* prev; // 8 bytes
 	int bitmap[16]; // 64 bytes
 };
 
@@ -22,10 +23,10 @@ typedef struct special_page_header {
 // if smallest size is 8, then most bytes we need is 64, which is 16 ints
 const int BITMAP_LENGTH = 16;
 const int BITS_PER_INT = 8 * sizeof(int);
-const int BIGGEST_SIZE = 4016;
+const int BIGGEST_SIZE = 4008;
 // sizes for easy lookup
 const size_t sizes[19] = { 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512,
-	768, 1024, 1536, 2048, 3192, 4016 };
+	768, 1024, 1536, 2048, 3192, 4008 };
 // array of pointers to page headers
 static page_header* bins[19];
 const size_t PAGE_SIZE = 4096;
@@ -34,6 +35,10 @@ static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 // 20 buckets = { 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256, 
 // 384, 512, 768, 1024, 1536, 2048, 3192, 4016}
+
+// 0 is free, 1 is full/unusable
+// so if int = 0, whole page is free
+// if int = -1, whole page is full
 
 
 
@@ -128,9 +133,11 @@ init_header(size_t bytes, page_header* passed)
 
 	header->next = 0;
 	if (passed == 0) {
+		header->prev = 0;
 		// this is first one, put in spot in bin
 		bins[find_bucket_index(bytes)] = header;
 	} else {
+		header->prev = passed;
 		passed->next = header;
 	}
 
@@ -146,7 +153,6 @@ get_usable_header(page_header* header) {
 	if (header == 0) {
 		return 0;
 	}
-	//printf("%li\n", header->size);
 	for (int ii = 0; ii < BITMAP_LENGTH; ii++) {
 		if (header->bitmap[ii] != -1) {
 			return header;
@@ -174,6 +180,16 @@ find_first_free(page_header* header)
 	return -1;
 }
 
+page_header*
+last_bucket(page_header* header) {
+	if (header == 0) {
+		return 0;
+	}
+	if (header->next) {
+		return last_bucket(header->next);
+	}
+	return header;
+}
 
 void*
 xmalloc(size_t bytes)
@@ -184,7 +200,7 @@ xmalloc(size_t bytes)
 		special_page_header* sph = mmap(NULL, bytes, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
 		sph->size = bytes;
 		// my birthday :)
-		sph->proof = 05152000;
+		sph->proof = 19405152000;
 		pthread_mutex_unlock(&(lock));
 		return ((void*) sph) + sizeof(special_page_header);
 	}
@@ -195,7 +211,10 @@ xmalloc(size_t bytes)
 	size_t offset = sizeof(page_header);
 	// if there is no header with space
 	if (header == 0) {
-		header = init_header(find_bucket_size(bucket), bins[bucket]);
+		// we should be passing in last_bucket(bins[bucket]) instead of header - but that 
+		// causes it to slow down tremendously in order to find the last one and it also 
+		// seems to not break when you just pass header, so whatever
+		header = init_header(find_bucket_size(bucket), header);
 		// since we now passing first block
 		toggle_bitmap(header, 0);
 		pthread_mutex_unlock(&(lock));
@@ -242,7 +261,7 @@ xfree(void* ptr)
 	pthread_mutex_lock(&(lock));
 	void* ptr_b = ptr - sizeof(size_t);
 	size_t thesize = *((size_t*) ptr_b);
-	if (thesize == 05152000) {
+	if (thesize == 19405152000) {
 		ptr_b -= sizeof(size_t);
 		size_t size = *((size_t*) ptr_b);
 		munmap(ptr_b, size);
@@ -256,10 +275,17 @@ xfree(void* ptr)
 	// toggle the bitmap at that index
 	toggle_bitmap(header, idx);
 	if (can_remap(header)) {
-		if (header->next == 0) {
-			bins[find_bucket_index(header->size)] = 0;
-		} else {
-			bins[find_bucket_index(header->size)] = header->next;
+		int hdr_idx = find_bucket_index(header->size);
+		if (header->prev == 0 && header->next == 0) {
+			bins[hdr_idx] = 0;
+		} else if (header->prev == 0 && header->next) {
+			(header->next)->prev = 0;
+			bins[hdr_idx] = header->next;
+		} else if (header->prev && header->next) {
+			(header->next)->prev = header->prev;
+			(header->prev)->next = header->next;
+		} else if (header->prev && header->next == 0) {
+			(header->prev)->next = 0;
 		}
 		munmap(header, PAGE_SIZE);
 
@@ -270,9 +296,13 @@ xfree(void* ptr)
 void*
 xrealloc(void* prev, size_t bytes)
 {
+	uintptr_t pt = find_closest_pointer((uintptr_t) prev);
+	page_header* header = (page_header*) pt;
+	size_t size = header->size;
 	void* new_space = xmalloc(bytes);
-	memcpy(new_space, prev, bytes);
+	//pthread_mutex_lock(&lock);
+	memcpy(new_space, prev, size);
+	//pthread_mutex_unlock(&lock);
 	xfree(prev);
 	return new_space;
 }
-
